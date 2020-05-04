d3ca5a6fe6809b9bb9f91acfd08da1e9acbd3dab
suryansh <suryanshsharma132@gmail.com>
2020-05-04 13:05:18 +0530

    week1

---
+++ week1/day4.cpp
@@ -0,0 +1,42 @@
+/* Number Complement
+Solution
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+ 
+
+Example 1:
+
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+ 
+
+Example 2:
+
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+ 
+
+Note:
+
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+
+*/
+
+
+//Approach is to find the number of bits in the number which is = log2(n)+1
+//then return xor of the number with pow(2,number_of_bits) -1 ;
+//this gives us the complement;
+
+class Solution {
+public:
+    int findComplement(int num) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);    
+        int number_of_bits = floor(log2(num))+1;
+        int n = pow(2,number_of_bits)-1;
+        return num^n;    
+    }
+};
